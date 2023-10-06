783cb5105cbb75d40d03d0b491266a87781489ae
su <soodoshll@gmail.com>
2023-10-06 16:29:47 -0400

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 python scripts/benchmark.py \
-       --model facebook/opt-6.7b \
-       --assist-model facebook/opt-125m \
-       --tokenizer facebook/opt-125m \
+       --model decapoda-research/llama-7b-hf  \
+       --assist-model JackFram/llama-160m  \
+       --tokenizer hf-internal-testing/llama-tokenizer \
        --len-out 128 \
-       --speculate-step 4 \
-       --batch-size 8 \
+       --speculate-step 0 1 2 3 4 5 6 7 8\
+       --batch-size 1 2 4 8 16 32\
        --fp16 \
        --dataset alespalla/chatbot_instruction_prompts \
        --dataset-truncate 200 \
